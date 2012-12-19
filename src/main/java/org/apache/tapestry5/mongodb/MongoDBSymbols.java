bc9e0a46eace7e17556b69a496ee86737f98dc02
Massimo Lusetti <mlusetti@gmail.com>
2012-12-19 17:43:09 +0100

    symbols for mongo secure mode

--- src/main/java/org/apache/tapestry5/mongodb/MongoDBSymbols.java
+++ src/main/java/org/apache/tapestry5/mongodb/MongoDBSymbols.java
@@ -16,7 +16,7 @@ public class MongoDBSymbols
     public static final String WRITE_CONCERN = "tapestry.mongodb.write_concern";
 
     /**
-     * The {@link com.mongodb.ReadPreference} to use. Default to {@link com.mongodb.ReadPreference.PRIMARY}.
+     * The {@link com.mongodb.ReadPreference} to use. Default to {@link com.mongodb.ReadPreference.primary()}.
      */
     public static final String READ_PREFERENCE = "tapestry.mongodb.read_preference";
 
@@ -56,4 +56,26 @@ public class MongoDBSymbols
      * Where to use or not the <em>consisten request</em> paradigm. Default to false;
      */
     public static final String CONSISTENT_REQUEST = "tapestry.mongodb.consistent_request";
+
+	// ########################################################################
+
+	/**
+	 * <code>true</code> if you need to connect to Mongo DB in <em>secure mode</em>,
+	 * <code>false</code> otherwise.
+	 * You need to set DB_USERNAME and DB_PASSWORD too if this is set to <code>true</code>.
+	 * Defaults to <code>false</code>
+	 */
+	public static final String SECURE_MODE = "tapestry.mongodb.secure_mode";
+
+	/**
+	 * Username to use to connect Mongo DB.
+	 * Defaults to empty string.
+	 */
+	public static final String DB_USERNAME = "tapestry.mongodb.db_username";
+
+	/**
+	 * Password to use to connect Mongo DB.
+	 * Defaults to empty string.
+	 */
+	public static final String DB_PASSWORD = "tapestry.mongodb.db_password";
 }
